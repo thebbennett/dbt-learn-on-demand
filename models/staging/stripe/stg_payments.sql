f6fa8afecc0b0e53a63117c7e219ade688bc7e36
Brittany Bennett <brittany@sunrisemovement.org>
2020-12-30 19:10:08 +0000

    refactor/dim_customers changed amount value

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ with base as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as order_status,
-        case when order_status = 'fail' then  amount * -1 else amount end as amount,
+        case when order_status = 'fail' then  (amount * -1)::decimal/100::decimal else amount::decimal/100::decimal end as amount,
         created::date as order_created_at 
     
     from sunrise.stripe_payments
